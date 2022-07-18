de6c68662f3129c4302b9138d0d99e380e4b417f
Chris Angelico <rosuav@gmail.com>
2022-07-18 11:07:50 +1000

    List line numbers on error messages

--- build.py
+++ build.py
@@ -5,8 +5,15 @@ import os
 import sys
 import yaml # ImportError? pip install pyyaml
 
+class Loader(yaml.SafeLoader):
+	"""Modified version of SafeLoader that retains line numbers"""
+	def construct_mapping(self, node, deep=False):
+		mapping = super().construct_mapping(node, deep=deep)
+		mapping["_line"] = node.start_mark.line + 1
+		return mapping
+
 with open("filelist.yaml") as f:
-	files = list(yaml.safe_load_all(f))
+	files = list(yaml.load_all(f, Loader))
 
 # Validate. Check that every YAML entry has the necessary attributes, and that
 # the files and YAML entries correspond correctly.
@@ -14,19 +21,23 @@ with open("filelist.yaml") as f:
 media = {f: False for f in os.listdir("media")}
 
 for f in files:
+	if not isinstance(f, dict) or "_line" not in f:
+		print("filelist.yaml:1: Malformed YAML entry", file=sys.stderr)
+		import pprint; pprint.pprint(f, stream=sys.stderr)
+		sys.exit(1)
 	if {"Filename", "License", "Type", "Description"} - set(f):
-		print("filelist.yaml:1: YAML entry lacks key attribute", file=sys.stderr)
+		print("filelist.yaml:%d: YAML entry lacks key attribute" % f["_line"], file=sys.stderr)
 		import pprint; pprint.pprint(f, stream=sys.stderr)
 		sys.exit(1)
 	if f["Filename"] not in media:
-		print("filelist.yaml:1: File listed in YAML but not found: %r" % f["Filename"], file=sys.stderr)
+		print("filelist.yaml:%d: File does not exist: %r" % (f["_line"], f["Filename"]), file=sys.stderr)
 		sys.exit(1)
 	if media[f["Filename"]]:
-		print("filelist.yaml:1: File listed twice in YAML: %r" % f["Filename"], file=sys.stderr)
+		print("filelist.yaml:%d: File listed twice in YAML: %r" % (f["_line"], f["Filename"]), file=sys.stderr)
 		sys.exit(1)
 	if f["Type"] not in {"Image", "Audio"}:
 		# Does Video also need to be represented separately?
-		print("filelist.yaml:1: File has invalid type: %r -> %r" % (f["Filename"], f["Type"]), file=sys.stderr)
+		print("filelist.yaml:%d: File has invalid type: %r -> %r" % (f["_line"], f["Filename"], f["Type"]), file=sys.stderr)
 		sys.exit(1)
 	media[f["Filename"]] = True
 	
