4e6d48b7279b6dc7387a075f57c14cdb245a5aae
Chris Angelico <rosuav@gmail.com>
2022-07-18 10:51:04 +1000

    Per TODO, validate the YAML data

--- build.py
+++ build.py
@@ -1,14 +1,37 @@
 import json
+import os
+import sys
 import yaml # ImportError? pip install pyyaml
 
 with open("filelist.yaml") as f:
 	files = list(yaml.safe_load_all(f))
 
-# TODO: Validate
-# Ensure that the files all exist
-# Ensure that no files exist in media/ that are not listed
-# Check that every file has the appropriate four attributes
-# Check that the Type is a valid type
+# Validate. Check that every YAML entry has the necessary attributes, and that
+# the files and YAML entries correspond correctly.
+
+media = {f: False for f in os.listdir("media")}
+
+for f in files:
+	if {"Filename", "License", "Type", "Description"} - set(f):
+		print("YAML entry lacks key attribute:", file=sys.stderr)
+		import pprint; pprint.pprint(f, stream=sys.stderr)
+		sys.exit(1)
+	if f["Filename"] not in media:
+		print("File listed in YAML but not found: %r" % f["Filename"], file=sys.stderr)
+		sys.exit(1)
+	if media[f["Filename"]]:
+		print("File listed twice in YAML: %r" % f["Filename"], file=sys.stderr)
+		sys.exit(1)
+	if f["Type"] not in {"Image", "Audio"}:
+		# Does Video also need to be represented separately?
+		print("File has invalid type: %r -> %r" % (f["Filename"], f["Type"]), file=sys.stderr)
+		sys.exit(1)
+	media[f["Filename"]] = True
+	
+for fn, seen in media.items():
+	if not seen:
+		print("File not listed in YAML: %r" % fn, file=sys.stderr)
+		sys.exit(1)
 
 # Write out the JSON file
 data = {"files": [
